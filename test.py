ff16bfc05e1884b35d65d39af4168c5c93358a0e
Samuel <sjhale@my.waketech.edu>
2020-11-08 19:52:05 -0500

    .

--- test.py
+++ test.py
@@ -3,20 +3,18 @@ import requests
 import time
 from selenium import webdriver
 
-driver = webdriver.Chrome('chromedriver')  # Optional argument, if not specified will search path.
-driver.get('https://en.wikipedia.org/');
-time.sleep(5) # Let the user actually see something!
-search_box = driver.find_element_by_name('search')
-search_box.send_keys('wake tech')
-search_box.submit()
-time.sleep(5) # Let the user actually see something!
-driver.quit()
-
-
-
-
 def test_wikipedia():
-    print("test")
+    driver = webdriver.Chrome('chromedriver')  # Optional argument, if not specified will search path.
+    driver.get('https://en.wikipedia.org/');
+    time.sleep(5) # Let the user actually see something!
+    search_box = driver.find_element_by_name('search')
+    search_box.send_keys('wake tech')
+    search_box.submit()
+    time.sleep(5) # Let the user actually see something!
+    title = driver.find_element_by_id('firstHeading')
+    assert (title.text == "Wake Technical Community College")
+    driver.quit()
+
 
 
 
